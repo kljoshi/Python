9015a6fe641e8d778e62e2b3d01c9ac5c34139e5
kljoshi <kapillaljoshi@gmail.com>
2019-02-16 11:28:08 -0600

    First commit: Note on installing thrid-party module and Exercise file on python concept for reference

---
+++ Exercise/if_else_example.py
@@ -0,0 +1,10 @@
+name = 'Bob'
+age = 3000
+if name == 'Allice':
+    print('Hi Allice')
+elif age < 12:
+    print('You are not Alice, Kiddo.')
+elif age > 2000:
+    print('Unlike you, Allice is not a undead, immortal vampire')
+elif age > 100:
+    print('You are not Alice, grannie')
---
+++ Exercise/if_example.py
@@ -0,0 +1,5 @@
+name = 'Bob'
+# indentation is called block
+if name == 'Allice':
+    print ('Hi Allice')
+print('Done')
